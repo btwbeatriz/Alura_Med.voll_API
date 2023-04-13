98eb1e5da4f2dd6f4379dd2968d2714169b44b0c
btwbeatriz <rm95246@fiap.com.br>
2023-04-13 14:18:35 -0300

    agendamento e exclusão de consultas adicionado

---
+++ src/main/java/med/voll/api/controller/consulta/DadosCancelamentoConsulta.java
@@ -0,0 +1,11 @@
+package med.voll.api.controller.consulta;
+
+import jakarta.validation.constraints.NotNull;
+
+public record DadosCancelamentoConsulta(
+        @NotNull
+        Long idConsulta,
+
+        @NotNull
+        MotivoCancelamento motivo) {
+}
